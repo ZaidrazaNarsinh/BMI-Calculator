96bb82c738bf4c75b27c7a9ebf2581333d73dfa1
ZaidNarsinh <zaidnarsinh@gmail.com>
2021-07-07 18:38:08 +0530

    BMI-Calculator-flutter

---
+++ android/app/src/main/java/com/zaidraza/bmicalculator/MainActivity.java
@@ -0,0 +1,7 @@
+package com.zaidraza.bmicalculator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+}
---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+
+  BottomButton({@required this.buttonTitle, @required this.onTap});
+
+  final String buttonTitle;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(child: Text(buttonTitle,style: kLargeButtonTextStyle,)),
+
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        height: kBottomContainerHeight,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: kBottomContainerColour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
